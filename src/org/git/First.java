15dc3e959c355c9f101649edeea4cc4c5925cab7
Jaikumar1324 <S.ravijaikumar@gmail.com>
2022-04-05 13:29:09 +0530

    sucess

--- src/org/git/First.java
+++ src/org/git/First.java
@@ -30,4 +30,18 @@ public class First {
 		System.out.println("6");
 
 	}
+	public void method10() {
+		System.out.println("10");
+
+	}
+	
+	public void method11() {
+		System.out.println("11");
+
+	}
+	
+	public void method12() {
+		System.out.println("12");
+
+	}
 }
